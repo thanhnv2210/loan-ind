a198ed7e8cffc6ead3cd5ce3e793aaefa951a6fc
thanhnv2210 <47849090+thanhnv2210@users.noreply.github.com>
2019-02-22 14:49:10 +0700

    Add files via upload

---
+++ src/main/java/vn/com/loan/ind/loanind/LoanIndApplication.java
@@ -0,0 +1,13 @@
+package vn.com.loan.ind.loanind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanIndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoanIndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vn/com/loan/ind/loanind/LoanIndApplicationTests.java
@@ -0,0 +1,16 @@
+package vn.com.loan.ind.loanind;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LoanIndApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
